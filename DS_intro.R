e4c84d78a9ea35ae9c01582644282c9e2ff0c05d
Fernando Caballero <cabafer1@gmail.com>
2023-01-31 03:00:13 -0300

    first commit

---
+++ DS_intro.R
@@ -0,0 +1,22 @@
+# clear environment
+rm(list=ls())
+
+# set work directory
+setwd("C:/Users/fernando.caballero/Desktop/MUNDOS_E/intro R")
+getwd()
+
+# exercise 1
+4+5       # addition  
+27-11     # subtraction
+3*9       # multiplication
+23/4      # division
+23%/%4    # integer part division
+23%%4     # mod  
+2/3+4/7   # addition of fractions  
+sqrt(36)  # square root
+sqrt(79)  # square root
+sin(pi)   # sine of pi  
+sin(3.14) # sine of pi
+
+
+
